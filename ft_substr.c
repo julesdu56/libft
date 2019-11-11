63a09d9f526949751a720184bd1b25a3e27003e9
Jules MOUROT <jumourot@e1r6p2.42.fr>
2019-11-11 14:58:54 +0100

    df

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jumourot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:07:39 by jumourot          #+#    #+#             */
-/*   Updated: 2019/11/08 13:54:25 by jumourot         ###   ########.fr       */
+/*   Updated: 2019/11/11 14:58:31 by jumourot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (start > len)
-		return (NULL);
-	if (s == NULL || !(str = ft_strnew(len)))
-		return (NULL);
-	while (s[start] && i < len)
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (s)
 	{
-		str[i] = s[start];
-		i++;
-		start++;
+		str = ft_strnew(len);
+		while (str == NULL)
+			return (NULL);
+		s = s + start;
+		while (s[start] && i < len)
+		{
+			str[i] = s[i];
+			i++;
+		}
+		return (str);
 	}
-	str[i] = '\0';
-	return (str);
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jumourot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 07:12:48 by jumourot          #+#    #+#             */
-/*   Updated: 2019/11/08 12:34:56 by jumourot         ###   ########.fr       */
+/*   Updated: 2019/11/11 09:53:03 by jumourot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+
+typedef	struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int					ft_count_digit(int n);
 void				*ft_memset(void	*b, int		c, size_t	len);
@@ -78,20 +84,15 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-typedef	struct			s_list
-{
-		void			*content;
-		struct s_list	*next;
-}						t_list;
-int			ft_lstsize(t_list *lst);
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-void		ft_lstadd_back(t_list **alst, t_list *new);
-void		ft_lstadd_front(t_list **alst, t_list *new);
-t_list		*ft_lstlast(t_list *lst);
-t_list		*ft_lstnew(void *content);
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *),
-			void (*del)(void *));
+int					ft_lstsize(t_list *lst);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+void				ft_lstadd_back(t_list **alst, t_list *new);
+void				ft_lstadd_front(t_list **alst, t_list *new);
+t_list				*ft_lstlast(t_list *lst);
+t_list				*ft_lstnew(void *content);
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+		void (*del)(void *));
 
 #endif
